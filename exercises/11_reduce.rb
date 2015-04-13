cae3b7471e8c8371885dff854e9080b1a552c5bb
Jake Marwil <rjm02006@gmail.com>
2015-04-13 13:22:50 -0600

    shorten formula

--- exercises/11_reduce.rb
+++ exercises/11_reduce.rb
@@ -1,35 +1,35 @@
-def reduce(input1, input2, &block)
-  a = input2
-  b = []
-  if input2 == 0
-    input1.each do |i|
-      a += i
-    end
-    block.call(a, input2)
-  elsif input2 == 1
-    input1.each do |i|
-      a *= i
-    end
-    block.call(a, input2)
-  elsif input2 == []
-    input1.each do |i|
-      b << block.call(a, i)
-    end
-    b.flatten
-  elsif input1 == []
-    input2
-  end
-end
-
-# def reduce(call, acc, &block)
-#   result = nil
-#
-#   call.each do |item|
-#     block.call(acc, item)
+# def reduce(input1, input2, &block)
+#   a = input2
+#   b = []
+#   if input2 == 0
+#     input1.each do |i|
+#       a += i
+#     end
+#     block.call(a, input2)
+#   elsif input2 == 1
+#     input1.each do |i|
+#       a *= i
+#     end
+#     block.call(a, input2)
+#   elsif input2 == []
+#     input1.each do |i|
+#       b << block.call(a, i)
+#     end
+#     b.flatten
+#   elsif input1 == []
+#     input2
 #   end
-#
-#   result
 # end
+
+def reduce(call, init, &block)
+  acc = init
+
+  call.each do |item|
+    acc = block.call(acc, item)
+  end
+
+  acc
+end
 
 # ------ code above this line ------
 
